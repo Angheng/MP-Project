514735e0cf4f95ed37e6c3af2484842c2d61e40e
Angheng <bungae1112@gmail.com>
2019-11-25 17:36:18 +0900

    Separating received message

--- app/src/main/java/com/bungae1112/final_proj/Firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/bungae1112/final_proj/Firebase/MyFirebaseMessagingService.java
@@ -1,12 +1,13 @@
 package com.bungae1112.final_proj.Firebase;
 
-import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -26,6 +27,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+        Map<String, String> data = remoteMessage.getData();
 
         Log.d(TAG, "Message Received:\t" + remoteMessage);
     }
