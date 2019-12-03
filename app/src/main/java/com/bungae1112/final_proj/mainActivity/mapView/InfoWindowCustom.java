fc714da2531976760eb0e5a2abd44ac8e62ede88
서민주 <minju1307@gmail.com>
2019-12-04 03:52:24 +0900

    Fix image on map info window

--- app/src/main/java/com/bungae1112/final_proj/mainActivity/mapView/InfoWindowCustom.java
+++ app/src/main/java/com/bungae1112/final_proj/mainActivity/mapView/InfoWindowCustom.java
@@ -10,8 +10,10 @@ import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.bungae1112.final_proj.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
@@ -33,11 +35,19 @@ public class InfoWindowCustom implements GoogleMap.InfoWindowAdapter {
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.info_window, null);
 
+        String img_url = "http://54.180.153.64:3000/images/" + marker.getTitle() + ".jpg";
+        Log.d("IMAGE LOAD", "IMAGE LOADING FROM" + img_url);
 
+
+        ImageView imageView = (ImageView) v.findViewById(R.id.storeImg);
         TextView title = (TextView) v.findViewById(R.id.nameTxt);
         TextView seats = (TextView) v.findViewById(R.id.seatsTxt);
+
+        Glide.with( inflater.getContext() ).load(img_url).into(imageView);
+
         title.setText(marker.getTitle());
         seats.setText(marker.getSnippet());
+
         return v;
     }
 
